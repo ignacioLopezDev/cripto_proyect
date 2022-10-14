a1f05c485b7c10dd5dd8fa25000e1ba74eebe7ff
ignacioLopezDev <ignaciolopez@outlook.com>
2022-10-14 09:43:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import Grid from "./components/Grid";
 
 function App() {
+  const api =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+    // getData - pedido axios.get de la api
+    const getData = async () => {
+      const res = await axios.get(api);
+      console.log(res.data);
+    };
+
+
+    // UseEffect - cuando cargue pagina traiga getData
+    useEffect(() => {
+      getData();
+    }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div>Cripto App</div>
+        <Routes>
+          <Route to="/" element={<Grid />} />
+        </Routes>
+        <Grid />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Grid.jsx
@@ -0,0 +1,9 @@
+function Grid() {
+  return (
+    <div>
+      <h3>Grid</h3>
+    </div>
+  );
+}
+
+export default Grid;
