8139c2ac8550cf1170d839699d2ca564ff7f5cbe
ignacioLopezDev <ignaciolopez@outlook.com>
2022-10-31 17:33:43 -0300

    second

--- api/models/index.model.js
+++ api/models/index.model.js
@@ -1,7 +1,7 @@
 const User = require("./User")
 const Favorite = require("./Favorite")
 
-User.belongsToMany(Favorite, {through: "UserId"})
-Favorite.belongsToMany(User, {through: "FavoriteId"})
+User.belongsToMany(Favorite, {through: "UserFavorites"})
+Favorite.belongsToMany(User, {through: "UserFavorites"})
 
 module.exports = {User, Favorite}
