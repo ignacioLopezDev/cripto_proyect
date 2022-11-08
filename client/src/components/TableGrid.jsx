aa88e3b0defacb123a8152f5aa798c21f968d810
ignacioLopezDev <ignaciolopez@outlook.com>
2022-11-08 19:18:59 -0300

    new

--- client/src/components/TableGrid.jsx
+++ client/src/components/TableGrid.jsx
@@ -51,70 +51,72 @@ const TableGrid = ({ criptoList }) => {
   const percent = `price_change_percentage_${chPercent}_in_currency`;
 
   return (
-
     // <div className="">
-    <div className="HomePage"> 
-    
-    <div id="HP-Grid-1" className="container">
-      <Searcher setSearch={setSearch} />
-      <table className="table table-dark mt-4 table-hover">
-        <thead>
-          <tr className="text-center">
-            <th style={{ width: "5%" }}></th>
-            <th style={{ width: "5%" }}></th>
-            <th style={{ width: "auto" }}>Crypto Coin</th>
-            <th style={{ width: "20%" }}>Price</th>
-            <th style={{ width: "24%" }}>% Percent</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filterCoins.map((coin, index) => (
-            <tr
-              key={index}
-              onClick={() => {
-                handleClick(coin.name.toLowerCase().replace(" ", "-"));
-              }}
-            >
-              <td className="text-muted text-center tableprop">
-                {coin.market_cap_rank}
-              </td>
-              <td className="tableprop">
-                <img
-                  src={coin.image}
-                  alt={coin.name}
-                  style={{ width: "30px" }}
-                />
-              </td>
-              <td>
-                <div>
-                  <span>{coin.name}</span>
-                  <div></div>
-                  <span className="text-muted text-uppercase">
-                    {coin.symbol}
-                  </span>
-                </div>
-              </td>
-              <td className="tableprop text-end">
-                {coin.current_price.toLocaleString("es-MX")}
-              </td>
-              <td
-                className={
-                  coin[percent] > 0
-                    ? "text-success text-center tableprop"
-                    : "text-danger text-center tableprop"
-                }
-              >
-                {Math.round(coin[percent] * 100) / 100}%
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    <div id="HP-Grid-2">
-      <div id="HP-Grid-2-1">hola</div>
-      <div id="HP-Grid-2-2">chau</div>
-    </div>
+    <div className="HomePage">
+      <div id="HP-Grid-1">
+        <div id="HP-Grid-1-1">
+        <Searcher  setSearch={setSearch} />
+        </div>
+        <div id="HP-Grid-1-2">
+          <table className="table table-dark mt-4 table-hover">
+            <thead>
+              <tr className="text-center">
+                <th style={{ width: "5%" }}></th>
+                <th style={{ width: "5%" }}></th>
+                <th style={{ width: "auto" }}>Crypto Coin</th>
+                <th style={{ width: "20%" }}>Price</th>
+                <th style={{ width: "24%" }}>% Percent</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterCoins.map((coin, index) => (
+                <tr
+                  key={index}
+                  onClick={() => {
+                    handleClick(coin.name.toLowerCase().replace(" ", "-"));
+                  }}
+                >
+                  <td className="text-muted text-center tableprop">
+                    {coin.market_cap_rank}
+                  </td>
+                  <td className="tableprop">
+                    <img
+                      src={coin.image}
+                      alt={coin.name}
+                      style={{ width: "30px" }}
+                    />
+                  </td>
+                  <td>
+                    <div>
+                      <span>{coin.name}</span>
+                      <div></div>
+                      <span className="text-muted text-uppercase">
+                        {coin.symbol}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="tableprop text-end">
+                    {coin.current_price.toLocaleString("es-MX")}
+                  </td>
+                  <td
+                    className={
+                      coin[percent] > 0
+                        ? "text-success text-center tableprop"
+                        : "text-danger text-center tableprop"
+                    }
+                  >
+                    {Math.round(coin[percent] * 100) / 100}%
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div id="HP-Grid-2">
+        <div id="HP-Grid-2-1">hola</div>
+        <div id="HP-Grid-2-2">chau</div>
+      </div>
     </div>
     // </div>
   );
